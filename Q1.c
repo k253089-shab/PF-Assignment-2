137018e278df0ff1ff96684b6b9b96d13878f679
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-10-30 01:34:04 +0500

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+void addBook(int isbn[100], char titles[100][50], float prices[100], int quantities[100], int count);
+void processSale(int isbn[100], int quantities[100], int count);
+void lowStockReport(int isbn[100], char titles[100][50], float prices[100], int quantities[100], int count);
+    
+int main()
+{
+        int isbn[100], quantities[100], count=0;
+        char titles[100][50];
+        float prices[100];
+        int choice;
+        printf("\n--- Liberty Books Inventory System ---\n");
+        printf("1. Add New Book\n");
+        printf("2. Process a Sale\n");
+        printf("3. Generate Low-Stock Report\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch(choice){
+            case 1: 
+                addBook(isbn, titles, prices, quantities, count);
+                break;
+            case 2:
+                processSale(isbn, quantities, count);
+                break;
+            case 3:
+                lowStockReport(isbn, titles, prices, quantities, count);
+                break;
+            case 4:
+                printf("Exiting program...\n");
+                break;
+            default:
+                printf("Invlaid choice");
+        }
+        
+    return 0;
+}
+
+    void addBook(int isbn[100], char titles[100][50], float prices[100], int quantities[100], int count){
+        printf("Enter ISBN: ");
+        scanf("%d", &isbn[count]);
+        
+        printf("Enter title: ");
+        fgets(titles[count], sizeof(titles[count]), stdin);
+        
+        printf("Enter prices: ");
+        scanf("%f", &prices[count]);
+        
+        printf("Enter quantity: ");
+        scanf("%d", &quantities[count]);
+    
+        count++;
+    }   
+    
+void processSale(int isbn[100], int quantities[100], int count){
+    printf("Enter ISBN: ");
+    scanf("%d", isbn[count]);
+    
+    printf("Enter quantity: ");
+    scanf("%d", &quantities[count]);
+}
+
+void lowStockReport(int isbn[100], char titles[100][50], float prices[100], int quantities[100], int count){
+    printf("Low Stock Report");
+    for(int i=0; i<=count; i++){
+        if(quantities[i]<5){
+            printf("ISBN: %d | Title: %s | Price: %f | Quantity: %d", isbn[i], titles[i], prices[i], quantities[i]);
+        } else{
+            printf("All books have sufficient stock.");
+        }
+    }
+}
